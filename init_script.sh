1e1e95181a0daacd159199fb9d690f8476c166ba
santhku <santhku@gmail.com>
2023-03-15 13:44:34 +0530

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -1,2 +1,2 @@
-g++ /home/santhosh/Led_blink_src/*.cpp -o /home/santhosh/Led_blink_src/LED_CODE.o
-/home/santhosh/Led_blink_src/LED_CODE.o &
+g++ /home/work/Led_blink_src/*.cpp -o /home/santhosh/Led_blink_src/LED_CODE.o
+/home/work/Led_blink_src/LED_CODE.o &
